fa99c639ec1dff55bcb084c677d1ac53cafe9aba
Kalila Ingco <103094824+kalilaingco@users.noreply.github.com>
2024-09-29 17:02:15 -0700

    Update Greedy.cpp

--- GasStation.cpp
+++ GasStation.cpp
@@ -1,26 +1,38 @@
-class Solution {
-public:
-    int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int Gas = 0;
-        int Cost = 0;
-        for(int i=0 ; i< gas.size(); i++){ //add up all elements in array gas and cost
-            Gas = Gas + gas[i];
-            Cost = Cost + cost[i];
-            
-        }
-        if(Cost > Gas){ //if cost is more than gas, no solution possible so return -1
-            return -1;
-        }
-        else{
-        int MaxProfit = 0;
-        for (int i = 0; i< gas.size(); i++){   //find starting city that gives most gas per distance ratio
-            int Profit = gas[i] - cost[i];  
-            if (Profit > MaxProfit){
-                MaxProfit = i;
-            }
+#include <vector>
+#include <iostream>
+#include <string.h>
 
-        }
-        return MaxProfit;
+using namespace std;
+
+
+int main(){
+
+    vector<int> city_distances = {5, 25, 15, 10, 15};
+    vector<int> fuel = {1, 2, 1, 0, 3};
+    int mpg = 10;
+
+    int Fuel = 0;       //int to keep track of how much fuel is in the car
+    int Distance = 0;   //int to keep track of the distances between cities
+    int Mpg = 0;           //int to multiply with fuel 
+    int startCity = 0;      //int to return starting city
+    int leftoverFuel = 0;      //int for keeping track of excess fuel
+
+    for(int i=0 ; i< fuel.size(); i++){ //add up all elements in array gas and cost
+        Mpg = mpg * fuel[i]; //multiply mpg* fuel to get real number of gas 
+        leftoverFuel = Mpg - city_distances[i]; //subtract distance from mpg to get remaining fuel
+        
+        Fuel = leftoverFuel + (Mpg - city_distances[i]); 
+        if(Fuel < 0){ //if fuel is less than 0, not possible starting city so reset fuel and increment city by 1
+        Fuel = 0; //reset fuel
+        startCity = i + 1; //increment through array
         }
     }
-};
+
+    if(leftoverFuel < 0){ //if leftover fuel is less than 0, no possible solution so return -1
+        return -1;
+    } else{
+    cout << "You should start at the city indexed at: " + startCity;
+    return startCity;
+    }
+    
+}
