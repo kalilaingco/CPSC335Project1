fcf8bbcb2d4abd43fa2677803b73b0da106c9202
Kalila Ingco <103094824+kalilaingco@users.noreply.github.com>
2024-09-29 17:39:59 -0700

    Update Greedy.cpp

--- Greedy.cpp
+++ Greedy.cpp
@@ -1,4 +1,10 @@
-#include <vector>
+// Developer: Kali Ingco
+// Date: 09/29/2024
+// Assignment: Project 1
+// Class: CPSC 335 Section 04
+// Notes: Arrays city_distances and gas, as well as int mpg are already initialized for testing purposes, 
+// they can be changed for further testing. The algorithm used in this program is the Greedy Algorithm. 
+
 #include <iostream>
 #include <string.h>
 
@@ -7,32 +13,26 @@ using namespace std;
 
 int main(){
 
-    vector<int> city_distances = {5, 25, 15, 10, 15};
-    vector<int> fuel = {1, 2, 1, 0, 3};
+    int city_distances[5]= {5,25,15,10,15};
+    int gas[5]= {1,2,1,0,3};
     int mpg = 10;
 
-    int Fuel = 0;       //int to keep track of how much fuel is in the car
-    int Distance = 0;   //int to keep track of the distances between cities
+    int k = sizeof(city_distances) /sizeof(city_distances[0]);
+    int Gas = 0;       //int to keep track of how much gas is in the car
     int Mpg = 0;           //int to multiply with fuel 
-    int startCity = 0;      //int to return starting city
+    int city = 0;      //int to return starting city
     int leftoverFuel = 0;      //int for keeping track of excess fuel
 
-    for(int i=0 ; i< fuel.size(); i++){ //add up all elements in array gas and cost
-        Mpg = mpg * fuel[i]; //multiply mpg* fuel to get real number of gas 
-        leftoverFuel = Mpg - city_distances[i]; //subtract distance from mpg to get remaining fuel
+    for(int i=0 ; i < k; i++){ //add up all elements in array gas and cost
+        Mpg = mpg * gas[i]; //multiply mpg* fuel to get real number of gas 
+        leftoverFuel = Mpg - city_distances[i]; //subtract distance from mpg to get remaining gas
         
-        Fuel = leftoverFuel + (Mpg - city_distances[i]); 
-        if(Fuel < 0){ //if fuel is less than 0, not possible starting city so reset fuel and increment city by 1
-        Fuel = 0; //reset fuel
-        startCity = i + 1; //increment through array
+        Gas = leftoverFuel + (Mpg - city_distances[i]); //keeps track of gas
+        if(Gas < 0){ //if Gas is less than 0, not possible starting city so reset fuel and increment city by 1
+        Gas = 0; //reset Gas
+        city = i + 1; //increment through array
         }
     }
-
-    if(leftoverFuel < 0){ //if leftover fuel is less than 0, no possible solution so return -1
-        return -1;
-    } else{
-    cout << "You should start at the city indexed at: " + startCity;
-    return startCity;
-    }
-    
+    cout << city; //there is a guarantee for a starting city so no need to check for invalid case
+    return 0;
 }
